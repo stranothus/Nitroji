a809d649c138e1237472332849b0c85509821121
stranothus <stranothus@gmail.com>
2021-12-27 21:01:21 -0600

    DM functionality and latency ping

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,12 @@ export default {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with pong"),
+    DMs: true,
     execute: function(interaction) {
         interaction.reply("Pong!");
+        interaction.fetchReply().then(reply => reply.channel.send("Latency is " + (reply.createdTimestamp - interaction.createdTimestamp) + "ms"));
     },
     executeText: function(msg, args) {
-        msg.channel.send("Pong!");
+        msg.channel.send("Pong!").then(reply => reply.channel.send("Latency is " + (reply.createdTimestamp - msg.createdTimestamp) + "ms"));
     }
 }
