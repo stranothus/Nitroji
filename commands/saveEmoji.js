23206210f18c0b501a69eb9a46efc22da2985ab2
stranothus <stranothus@gmail.com>
2022-01-27 11:11:09 -0600

    Save emojis to database Users can save an emoji or an animated emoji to the database, connected to their user id and associated with a name.

---
+++ commands/saveEmoji.js
@@ -0,0 +1,121 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+ export default {
+    data: new SlashCommandBuilder()
+        .setName("saveemoji")
+        .setDescription("Saves an emoji")
+        .addStringOption(option => option
+            .setName("emoji")
+            .setDescription("The emoji from this server to save")
+            .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName("name")
+            .setDescription("The name to save the emoji as")
+            .setRequired(true)
+        ),
+    DMs: true,
+    execute: async function(interaction) {
+        const emoji = interaction.options.getString("emoji").trim();
+        const name = interaction.options.getString("name").trim();
+
+        if(!emoji.match(/:[^\s]+:\d{18}/)) {
+            interaction.reply({
+                content: "Please input a valid server emoji!",
+                ephemeral: true
+            });
+
+            return;
+        }
+
+        const emojiObject = await interaction.guild.emojis.fetch(emoji.match(/\d{18}/).reverse()[0]);
+        const emojisColl = await db.db("Users").collection("emojis");
+        const documentExists = await emojisColl.findOne({ user: interaction.member.id });
+        const emojiExists = await emojisColl.findOne({ user: interaction.member.id, emojis: { $elemMatch: { $or: [{ id: emojiObject.id }, { name: name }]}}});
+
+        if(documentExists) {
+            if(emojiExists) {
+                interaction.reply({
+                    content: "Looks like you already have that emoji or another emoji by the same name saved!",
+                    ephemeral: true
+                });
+    
+                return;
+            }
+
+            await emojisColl.updateOne({ user: interaction.member.id }, { $push: { emojis: { 
+                id: emojiObject.id,
+                name: name
+            }}});
+        } else {
+            await emojisColl.insertOne({
+                user: interaction.member.id,
+                emojis: [
+                    {
+                        id: emojiObject.id,
+                        name: name
+                    }
+                ]
+            });
+        }
+
+        interaction.reply({
+            content: `You saved <${emojiObject.animated ? "a" : ""}:${emojiObject.name}:${emojiObject.id}>! Access it through servers I'm in with \`!${name}!\` in your message`,
+            ephemeral: true
+        });
+    },
+    executeText: async function(msg, args) {
+        const emoji = args[0];
+        const name = args[1];
+
+        if(!emoji || !name) {
+            msg.reply({
+                content: "Please input an emoji and a name!"
+            });
+
+            return;
+        }
+
+        if(!emoji.match(/:[^\s]+:\d{18}/)) {
+            msg.reply({
+                content: "Please input a valid server emoji!"
+            });
+
+            return;
+        }
+
+        const emojiObject = await msg.guild.emojis.fetch(emoji.match(/\d{18}/).reverse()[0]);
+        const emojisColl = await db.db("Users").collection("emojis");
+        const documentExists = await emojisColl.findOne({ user: msg.member.id });
+        const emojiExists = await emojisColl.findOne({ user: msg.member.id, emojis: { $elemMatch: { $or: [{ id: emojiObject.id }, { name: name }]}}});
+
+        if(documentExists) {
+            if(emojiExists) {
+                msg.reply({
+                    content: "Looks like you already have that emoji or another emoji by the same name saved!"
+                });
+    
+                return;
+            }
+
+            await emojisColl.updateOne({ user: msg.member.id }, { $push: { emojis: { 
+                id: emojiObject.id,
+                name: name
+            }}});
+        } else {
+            await emojisColl.insertOne({
+                user: msg.member.id,
+                emojis: [
+                    {
+                        id: emojiObject.id,
+                        name: name
+                    }
+                ]
+            });
+        }
+
+        msg.reply({
+            content: `You saved <${emojiObject.animated ? "a" : ""}:${emojiObject.name}:${emojiObject.id}>! Access it through servers I'm in with \`!${name}!\` in your message`
+        });
+    }
+}
