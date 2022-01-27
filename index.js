d10b2c2990cc3bbc887faf76d266d8f799b857d6
stranothus <stranothus@gmail.com>
2022-01-27 08:33:33 -0600

    Implement global db Connect to the Mongo database and allow global access.

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 import discord from "discord.js";
 import dotenv from "dotenv";
 import dirFlat from "./utils/dirFlat.js";
+import { MongoClient } from "mongodb";
 
  // load env variables
 dotenv.config();
+
+global.db = await new Promise((resolve, reject) => {
+    MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rissn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        (err, db) => {
+            if(err) console.error(err);
+
+            console.log("DB connected");
+
+            resolve(db);
+        }
+    );
+});
 
  // create the Discord client with basic intents
 const client = new discord.Client({
