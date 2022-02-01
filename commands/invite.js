c4c5b9098f21feb7fa2243d6f009636129afa446
stranothus <stranothus@gmail.com>
2022-02-01 13:27:03 -0600

    Invite command

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+ // import package
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export default {
+    // setup the slash command format 
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("Replies with pong"),
+    // let it be used in DMs
+    DMs: true,
+    // execute for slash commands
+    execute: function(interaction) {
+        interaction.reply("https://discord.com/api/oauth2/authorize?client_id=936076862797520956&permissions=275415050240&scope=bot%20applications.commands");
+    },
+    // execute for text commands
+    executeText: function(msg, args) {
+        msg.channel.send("https://discord.com/api/oauth2/authorize?client_id=936076862797520956&permissions=275415050240&scope=bot%20applications.commands")
+    }
+}
